5622bfa44438ea21391da145f318a7185be5413a
Lilili <2874270572lll@gmail.com>
2026-05-17 16:18:12 +0800

    fix: 禁用 Home.jsx useEffect 依赖警告

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -14,6 +14,7 @@ const Home = () => {
 
   useEffect(() => {
     fetchTasks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activeCategory]);
 
   const fetchTasks = async () => {
